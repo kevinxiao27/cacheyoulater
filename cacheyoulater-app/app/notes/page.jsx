a03c52a02139a60b4beb6c6ded2f2c381418030c
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2024-01-20 14:07:02 -0800

    pocketbase testing

--- cacheyoulater-app/app/notes/page.jsx
+++ cacheyoulater-app/app/notes/page.jsx
@@ -1,7 +1,7 @@
 // import PocketBase from 'pocketbase';
-import Link from 'next/link';
-import styles from './Notes.module.css';
-import CreateNote from './CreateNote';
+import Link from "next/link"
+import styles from "./Notes.module.css"
+import CreateNote from "./CreateNote"
 
 // export const dynamic = 'auto',
 //   dynamicParams = true,
@@ -10,34 +10,36 @@ import CreateNote from './CreateNote';
 //   runtime = 'nodejs',
 //   preferredRegion = 'auto'
 
-
 async function getNotes() {
   // const db = new PocketBase('http://127.0.0.1:8090');
   // const result = await db.records.getList('notes');
-  const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30', { cache: 'no-store' });
-  const data = await res.json();
-  return data?.items;
+  const res = await fetch(
+    "http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30",
+    { cache: "no-store" }
+  )
+  const data = await res.json()
+  return data?.items
 }
 
 export default async function NotesPage() {
-  const notes = await getNotes();
+  const notes = await getNotes()
 
-  return(
+  return (
     <div>
       <h1>Notes</h1>
       <div className={styles.grid}>
         {notes?.map((note) => {
-          return <Note key={note.id} note={note} />;
+          return <Note key={note.id} note={note} />
         })}
       </div>
 
       <CreateNote />
     </div>
-  );
+  )
 }
 
 function Note({ note }) {
-  const { id, title, content, created } = note || {};
+  const { id, title, content, created } = note || {}
 
   return (
     <Link href={`/notes/${id}`}>
@@ -47,5 +49,5 @@ function Note({ note }) {
         <p>{created}</p>
       </div>
     </Link>
-  );
+  )
 }
