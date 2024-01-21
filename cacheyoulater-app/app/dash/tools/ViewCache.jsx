2b5fd7b12343e0c329f118244ac504efc834e1d9
Isaac Liu <liuisaac05@gmail.com>
2024-01-21 01:17:26 -0800

    bruh
    Changes to be committed:
    modified:   cacheyoulater-app/app/dash/page.jsx
    modified:   cacheyoulater-app/app/dash/tools/Box.jsx
    modified:   cacheyoulater-app/app/dash/tools/CreateCache.jsx
    modified:   cacheyoulater-app/app/dash/tools/ViewCache.jsx

--- cacheyoulater-app/app/dash/tools/ViewCache.jsx
+++ cacheyoulater-app/app/dash/tools/ViewCache.jsx
@@ -11,7 +11,7 @@ import {
     sample,
 } from "@/assets";
 
-const ViewCache = ({ buttonscaled }) => {
+const ViewCache = ({ buttonscaled, jsoninfo }) => {
     return (
         <div
             className={`transition duration-500 ease-in-out absolute z-50 overscroll-none overflow-hidden pointer-events-auto`}
@@ -39,14 +39,25 @@ const ViewCache = ({ buttonscaled }) => {
                 </div>
                 <div className="flex flex-col w-screen justify-center items-center mt-4 hover:cursor-pointer">
                     <div className="w-[80vw] h-[80vw] overflow-hidden">
+                        <span className="font-bold italic">
+                            {jsoninfo.title}
+                        </span>
                         <Image
                             src={sample}
                             alt={"create cache"}
-                            style={{objectFit:"contain"}}
+                            style={{ objectFit: "contain" }}
                         />
                     </div>
                 </div>
-                <span className="ml-[10vw] mt-8">Hint: Near where we met when we were nine!</span>
+                <div className="flex flex-col justify-center items-align">
+                    <span className="my-2 w-[80vw] mx-auto text-wrap">
+                        <span className="font-bold">Description: </span>
+                        {jsoninfo.description}
+                    </span>
+                    <span className="ml-[10vw]">
+                        <span className="font-bold">Hint: </span>Near where we met when we were nine!
+                    </span>
+                </div>
 
                 <div className="w-screen flex flex-row justify-center items-center z-50 mt-8">
                     <div className="rounded-full outline-2 outline-black sm:w-1/3 w-2/3 border-2 border-black">
@@ -58,7 +69,7 @@ const ViewCache = ({ buttonscaled }) => {
                         </button>
                     </div>
                 </div>
-                <div className="w-screen flex flex-row justify-center items-center z-10 hover:cursor-pointer mt-12">
+                <div className="w-screen flex flex-row justify-center items-center z-10 hover:cursor-pointer -mt-4">
                     <Image
                         src={map}
                         width={1000}
