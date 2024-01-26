bf3e1af8c491a78d151f5e10887455ff8df40e49
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2024-01-25 22:33:51 -0800

    Fix: Backend configured to use CORS

--- cacheyoulater-backend/app.js
+++ cacheyoulater-backend/app.js
@@ -1,11 +1,18 @@
 // import express from "express"
 // import mongoose, { connect } from "mongoose"
-import dotenv from "dotenv"
-import createServer from "./utils/server.js"
+import dotenv from "dotenv";
+import createServer from "./utils/server.js";
+import cors from "cors";
 
-dotenv.config()
-const app = createServer()
+dotenv.config();
+const app = createServer();
+
+let corsOptions = {
+  origin: ["http://localhost:3000"],
+};
+
+app.use(cors(corsOptions));
 
 app.listen(8080, async () => {
-  console.log("Connected to MongoDB and running at http://localhost:8080")
-})
+  console.log("Connected to MongoDB and running at http://localhost:8080");
+});
